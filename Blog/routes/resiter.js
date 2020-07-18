85091e9d88f4f2a2147888c041c7d784dbeea044
prakash-simhandri <prakashsimhadri.com@gmail.com>
2020-07-18 19:51:15 +0530

    some bugs

--- Blog/routes/resiter.js
+++ Blog/routes/resiter.js
@@ -1,21 +1,35 @@
 module.exports = (register, knex, ) => {
     register.post('/registration', (req, res) => {
         knex("users")
-            .insert({
-                email: req.body.email,
-                password: req.body.password
-            })
+        .select('*').where({"email" :req.body.email})
+        .then((data)=>{
+            if (data.length<0){
+                res.send('user already exist !')
+            }else{
+                knex('users')
+                .insert({
+                    email: req.body.email,
+                    password: req.body.password
+                })
+                
+                .then(() => {
+                    console.log("data fechted...");
+                    res.send("Reqistration Done!")
+    
+                })
+                .catch((err) => {
+                    console.log(err);
+                    res.send(err);
+    
+                })
+
+            }
+        })
+        .catch((err)=>{
+            console.log(err);
+            res.send(err)
+        })
             
-            .then(() => {
-                console.log("data fechted...");
-                res.send("Reqistration Done!")
-
-            })
-            .catch((err) => {
-                console.log(err);
-                res.send(err);
-
-            })
     })
 
 
